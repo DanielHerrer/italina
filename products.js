d6a6cd877edd22e9081984048495a1367b1a4f06
Herrera Daniel <danielherrera2009@hotmail.es>
2023-11-22 15:05:17 -0300

    modal detalles producto, añadiendo imagenes a productos, modificando js

--- products.js
+++ products.js
@@ -1,10 +1,11 @@
 // --------------------------------------------------------------
 
-// Definir productos
+// Traer datos de los productos
+// ( descripcion (Max. 800 caracteres) / imagenes: (Max. 4 fotos) )
 const productos = [
-    { id: 1, disponible: 'disponible', titulo: 'Kit Set completo de baño', precio: 5500, imagen: 'img/product_set.jpg', descripcion: 'El kit Set de Baño incluye una toalla suave de mano, pads quita maquillaje reutilizables, vincha para cabello y turbante absorbente.<br> Experimenta comodidad y funcionalidad en cada paso de tu rutina de cuidado personal. Un conjunto diseñado para mejorar tu experiencia despues de un baño. <ul class="texto-item fw-bold fs-5" style="list-style-type: circle;"><li>Turbante para cabello</li><li>Toalla de mano</li><li>Pads quita maquillaje</li><li>Vincha de pelo</li></ul>' },
-    { id: 2, disponible: 'disponible', titulo: 'Bata de toalla para baño', precio: 10500, imagen: 'img/product_bata.jpg', descripcion: '<span style="text-decoration: underline;">Absorción superior</span>: La bata de baño está hecha de materiales altamente absorbentes que te ayudarán a secarte rápidamente después de bañarte. <br><span style="text-decoration: underline;">Comodidad en el diseño</span>: Su diseño espacioso y corte relajado brindan una sensación de comodidad y libertad de movimiento. <br> <span style="text-decoration: underline;">Durabilidad</span>: Utilizamos materiales de alta calidad para garantizar que nuestras batas sean duraderas y resistan el desgaste constante. Puedes confiar en la calidad y la resistencia de nuestras batas para que te acompañen durante mucho tiempo.' },
-    { id: 3, disponible: 'no-disponible', titulo: 'Funda de toalla almohada ', precio: 2900, imagen: 'img/product_almohada.jpg', descripcion: '<span style="text-decoration: underline;">Compatibilidad rizado</span>: Recomendada especialmente para cabello rizado y ondulado, estas fundas son la respuesta al frizz no deseado. Ayudan a que tus rizos mantengan su forma y evitan el envejecimiento prematuro de tu cabello, preservando la integridad de tu peinado mientras se seca. <br><span style="text-decoration: underline;">Protección nocturna</span>: Nuestra funda de almohada está cuidadosamente diseñada para proteger tanto tu cabello como tu almohada mientras disfrutas de un sueño reparador con el cabello mojado.' },
+    { id: 1, disponible: 'disponible', titulo: 'Kit Set completo de baño', precio: 5500, imagenes: ['img/product_set.jpg', 'img/product_set02.jpg', 'img/product_set03.jpg', 'img/product_set04.jpg'], descripcion: 'El kit Set de Baño incluye una toalla suave de mano, pads quita maquillaje reutilizables, vincha para cabello y turbante absorbente.<br> Experimenta comodidad y funcionalidad en cada paso de tu rutina de cuidado personal. Un conjunto diseñado para mejorar tu experiencia despues de un baño. <ul class="texto-item fw-bold fs-5" style="list-style-type: circle;"><li>Turbante para cabello</li><li>Toalla de mano</li><li>Pads quita maquillaje</li><li>Vincha de pelo</li></ul>' },
+    { id: 2, disponible: 'disponible', titulo: 'Bata de toalla para baño', precio: 10500, imagenes: ['img/product_bata.jpg', 'img/product_bata02.jpg', 'img/product_bata03.jpg', 'img/product_bata04.jpg'], descripcion: '<span style="text-decoration: underline;">Absorción superior</span>: La bata de baño está hecha de materiales altamente absorbentes que te ayudarán a secarte rápidamente después de bañarte. <br><span style="text-decoration: underline;">Comodidad en el diseño</span>: Su diseño espacioso y corte relajado brindan una sensación de comodidad y libertad de movimiento. <br> <span style="text-decoration: underline;">Durabilidad</span>: Utilizamos materiales de alta calidad para garantizar que nuestras batas sean duraderas y resistan el desgaste constante. Puedes confiar en la calidad y la resistencia de nuestras batas para que te acompañen durante mucho tiempo.' },
+    { id: 3, disponible: 'no-disponible', titulo: 'Funda de toalla almohada ', precio: 2900, imagenes: ['img/product_almohada.jpg'], descripcion: '<span style="text-decoration: underline;">Compatibilidad rizado</span>: Recomendada especialmente para cabello rizado y ondulado, estas fundas son la respuesta al frizz no deseado. Ayudan a que tus rizos mantengan su forma y evitan el envejecimiento prematuro de tu cabello, preservando la integridad de tu peinado mientras se seca. <br><span style="text-decoration: underline;">Protección nocturna</span>: Nuestra funda de almohada está cuidadosamente diseñada para proteger tanto tu cabello como tu almohada mientras disfrutas de un sueño reparador con el cabello mojado.' },
 ];
 
 // Función para mostrar productos en la página
@@ -21,12 +22,12 @@ function mostrarProductos() {
                     </div>
                     <div class="producto-caja-vista">
                         <div class="producto-vista">
-                            <img src="${producto.imagen}" alt="${producto.titulo}" class="marco-disponible 
+                            <img src="${producto.imagenes[0]}" alt="${producto.titulo}" class="marco-disponible 
                             featurette-image img-fluid mx-auto" width="500" height="500" role="img"
                                 preserveAspectRatio="xMidYMid slice" focusable="false"></img>
                             <div class="producto-botones">
-                                <a href="product-detail-1.html"><button class="boton-producto boton-producto-ver"><i
-                                            class="fa-solid fa-circle-info"></i> Detalles</button></a>
+                                <button onclick="modalDetalles(${producto.id});" class="boton-producto boton-producto-ver"><i
+                                            class="fa-solid fa-circle-info"></i> Detalles</button>
                                 <button onclick="agregarAlCarrito(${producto.id}); notificacionCarrito(this, ${producto.id}); actualizarContadorCarrito();" 
                                     class="boton-producto boton-producto-carrito">
                                         <i class="fa-solid fa-cart-plus"></i></button>
@@ -47,7 +48,7 @@ function mostrarProductos() {
                     </div>
                     <div class="producto-caja-vista">
                         <div class="producto-vista">
-                            <img src="${producto.imagen}" alt="${producto.titulo}" class="marco-no-disponible 
+                            <img src="${producto.imagenes[0]}" alt="${producto.titulo}" class="marco-no-disponible 
                             featurette-image img-fluid mx-auto" width="500" height="500" role="img"
                                 preserveAspectRatio="xMidYMid slice" focusable="false"></img>
                             <div class="producto-botones">
@@ -142,6 +143,51 @@ function notificacionCarrito(boton, id) {
         });
     }, 2000); // 2000 milisegundos = 2 segundos
 }
+
+// ------------ Modal Detalles Producto
+
+function modalDetalles(id) {
+
+    const producto = productos.find(p => p.id === id);
+
+    const modalProducto = document.querySelector("#modal-producto");
+    const modalContenido = document.querySelector(".modal-producto-content");
+
+    modalProducto.showModal();
+
+    modalContenido.innerHTML = `
+        <div class="fotos-producto">
+            <img class="foto-max" src="${producto.imagenes[0]}" alt="">
+            <div class="barra-fotos">
+                <img class="foto-min" src="${producto.imagenes[0]}" alt="">
+                <img class="foto-min" src="${producto.imagenes[1]}" alt="">
+                <img class="foto-min" src="${producto.imagenes[2]}" alt="">
+                <img class="foto-min" src="${producto.imagenes[3]}" alt="">
+            </div>
+        </div>
+        <div class="descripcion-producto">
+            <h2 class="titulo-producto">${producto.titulo}</h2>
+            <p>${producto.descripcion}</p>
+        </div>
+    `;
+
+}
+
+function cerrarDetalles() {
+
+    const modalProducto = document.querySelector("#modal-producto");
+    modalProducto.close();
+
+}
+
+// btnAbrirModal.addEventListener("click", () => {
+//     modal.showModal();
+// });
+
+// btnCerrarModal.addEventListener("click", () => {
+//     modalProducto.close();
+// })
+
 
 // ------------ Evento para cargar el carrito al cargar la página
 
--- products_cart.js
+++ products_cart.js
@@ -1,8 +1,11 @@
-// Definir productos
+// --------------------------------------------------------------
+
+// Traer datos de los productos
+// ( descripcion (Max. 800 caracteres) / imagenes: (Max. 4 fotos) )
 const productos = [
-    { id: 1, disponible: 'disponible', titulo: 'Kit Set completo de baño', imagen: 'img/product_set.jpg', descripcion: 'El kit Set de Baño incluye una toalla suave de mano, pads quita maquillaje reutilizables, vincha para cabello y turbante absorbente. Experimenta comodidad y funcionalidad en cada paso de tu rutina de cuidado personal. Un conjunto diseñado para mejorar tu experiencia despues de un baño. <ul class="texto-item fw-bold fs-5" style="list-style-type: circle;"><li>Turbante para cabello</li><li>Toalla de mano</li><li>Pads quita maquillaje</li><li>Vincha de pelo</li></ul>' },
-    { id: 2, disponible: 'disponible', titulo: 'Batas de toalla para baño', imagen: 'img/product_bata.jpg', descripcion: '<span style="text-decoration: underline;">Absorción superior</span>: La bata de baño está hecha de materiales altamente absorbentes que te ayudarán a secarte rápidamente después de bañarte. <br><span style="text-decoration: underline;">Comodidad en el diseño</span>: Su diseño espacioso y corte relajado brindan una sensación de comodidad y libertad de movimiento. <br> <span style="text-decoration: underline;">Durabilidad</span>: Utilizamos materiales de alta calidad para garantizar que nuestras batas sean duraderas y resistan el desgaste constante. Puedes confiar en la calidad y la resistencia de nuestras batas para que te acompañen durante mucho tiempo.' },
-    { id: 3, disponible: 'no-disponible', titulo: 'Fundas de toalla almohada ', imagen: 'img/product_almohada.jpg', descripcion: '<span style="text-decoration: underline;">Compatibilidad rizado</span>: Recomendada especialmente para cabello rizado y ondulado, estas fundas son la respuesta al frizz no deseado. Ayudan a que tus rizos mantengan su forma y evitan el envejecimiento prematuro de tu cabello, preservando la integridad de tu peinado mientras se seca. <br><span style="text-decoration: underline;">Protección nocturna</span>: Nuestra funda de almohada está cuidadosamente diseñada para proteger tanto tu cabello como tu almohada mientras disfrutas de un sueño reparador con el cabello mojado.' },
+    { id: 1, disponible: 'disponible', titulo: 'Kit Set completo de baño', precio: 5500, imagenes: ['img/product_set.jpg', 'img/product_set02.jpg', 'img/product_set03.jpg', 'img/product_set04.jpg'], descripcion: 'El kit Set de Baño incluye una toalla suave de mano, pads quita maquillaje reutilizables, vincha para cabello y turbante absorbente.<br> Experimenta comodidad y funcionalidad en cada paso de tu rutina de cuidado personal. Un conjunto diseñado para mejorar tu experiencia despues de un baño. <ul class="texto-item fw-bold fs-5" style="list-style-type: circle;"><li>Turbante para cabello</li><li>Toalla de mano</li><li>Pads quita maquillaje</li><li>Vincha de pelo</li></ul>' },
+    { id: 2, disponible: 'disponible', titulo: 'Bata de toalla para baño', precio: 10500, imagenes: ['img/product_bata.jpg', 'img/product_bata02.jpg', 'img/product_bata03.jpg', 'img/product_bata04.jpg'], descripcion: '<span style="text-decoration: underline;">Absorción superior</span>: La bata de baño está hecha de materiales altamente absorbentes que te ayudarán a secarte rápidamente después de bañarte. <br><span style="text-decoration: underline;">Comodidad en el diseño</span>: Su diseño espacioso y corte relajado brindan una sensación de comodidad y libertad de movimiento. <br> <span style="text-decoration: underline;">Durabilidad</span>: Utilizamos materiales de alta calidad para garantizar que nuestras batas sean duraderas y resistan el desgaste constante. Puedes confiar en la calidad y la resistencia de nuestras batas para que te acompañen durante mucho tiempo.' },
+    { id: 3, disponible: 'no-disponible', titulo: 'Funda de toalla almohada ', precio: 2900, imagenes: ['img/product_almohada.jpg'], descripcion: '<span style="text-decoration: underline;">Compatibilidad rizado</span>: Recomendada especialmente para cabello rizado y ondulado, estas fundas son la respuesta al frizz no deseado. Ayudan a que tus rizos mantengan su forma y evitan el envejecimiento prematuro de tu cabello, preservando la integridad de tu peinado mientras se seca. <br><span style="text-decoration: underline;">Protección nocturna</span>: Nuestra funda de almohada está cuidadosamente diseñada para proteger tanto tu cabello como tu almohada mientras disfrutas de un sueño reparador con el cabello mojado.' },
 ];
 
 // Función para agregar productos al carrito
@@ -17,7 +20,8 @@ function agregarAlCarrito(id) {
         }
         mostrarCarrito();
         guardarCarritoEnLocalStorage();
-        actualizarContadorCarrito()
+        actualizarContadorCarrito();
+        actualizarPrecioTotal();
     }
 }
 
@@ -33,7 +37,8 @@ function reducirAlCarrito(id) {
         }
         mostrarCarrito();
         guardarCarritoEnLocalStorage();
-        actualizarContadorCarrito()
+        actualizarContadorCarrito();
+        actualizarPrecioTotal();
     }
 }
 
@@ -50,36 +55,64 @@ function cargarCarritoDesdeLocalStorage() {
     }
 }
 
-// Función para mostrar el carrito en el modal
+// Función para mostrar el carrito en la lista
 function mostrarCarrito() {
     const contenedorCarrito = document.getElementById('listaCarrito');
     contenedorCarrito.innerHTML = '';
-    carrito.forEach(item => {
+    if (carrito.length > 0) {
+        carrito.forEach(item => {
+            const itemHTML = `
+            <div class="item-carrito">
+                <div class="item-carrito-producto">
+                    <img class="item-img" src="${item.imagenes[0]}" alt="${item.titulo}">
+                    <p class="item-titulo">${item.titulo}</p>
+                </div>
+                <div class="item-carrito-precio">
+                    <p class="item-precio">$${item.precio} ARS</p>
+                </div>
+                <div class="item-carrito-cantidad">
+                    <button class="btn-carrito btn-mas" onclick="agregarAlCarrito(${item.id})"><i class="fa-solid fa-plus"></i></button>
+                    <p class="num-cantidad">Cantidad: ${item.cantidad}</p>
+                    <button class="btn-carrito btn-menos" onclick="reducirAlCarrito(${item.id})"><i class="fa-solid fa-minus"></i></button>
+                </div>
+                <div class="item-carrito-subtotal">
+                    <p class="item-subtotal">$${item.precio * item.cantidad} ARS</p>
+                </div>
+                <div class="item-carrito-borrar">
+                    <button class="btn-carrito item-btn-remover" onclick="quitarDelCarrito(${item.id})"><i class="fa-solid fa-trash-can"></i></button>
+                </div>
+            </div>
+            
+          `;
+            contenedorCarrito.innerHTML += itemHTML;
+        });
+    } else {
         const itemHTML = `
-        <div class="item-carrito">
-            <div class="item-carrito-producto">
-                <img class="item-img" src="${item.imagen}" alt="${item.titulo}">
-                <p class="item-titulo">${item.titulo}</p>
+            <div class="item-carrito" style="color: #747474;">
+                <div class="item-carrito-producto">
+                    <img class="item-sin-img" src="./img/question-secondary.png" style="filter: brightness(1.1);" alt="Item producto">
+                    <p class="item-titulo">Ingrese aquí su producto</p>
+                </div>
+                <div class="item-carrito-precio">
+                    <p class="item-precio">ARS</p>
+                </div>
+                <div class="item-carrito-cantidad">
+                    <button class="btn-carrito btn-mas" style="background-color: #ddd;"><i class="fa-solid fa-plus"></i></button>
+                    <p class="num-cantidad">Cantidad: ?</p>
+                    <button class="btn-carrito btn-menos" style="background-color: #ddd;"><i class="fa-solid fa-minus"></i></button>
+                </div>
+                <div class="item-carrito-subtotal">
+                    <p class="item-subtotal">ARS</p>
+                </div>
+                <div class="item-carrito-borrar">
+                    <button class="btn-carrito item-btn-remover" style="background-color: #ddd;"><i class="fa-solid fa-trash-can"></i></button>
+                </div>
             </div>
-            <div class="item-carrito-precio">
-                <p class="item-precio">$${item.precio} ARS</p>
-            </div>
-            <div class="item-carrito-cantidad">
-                <button class="btn-carrito btn-mas" onclick="agregarAlCarrito(${item.id})"><i class="fa-solid fa-plus"></i></button>
-                <p class="num-cantidad">Cantidad: ${item.cantidad}</p>
-                <button class="btn-carrito btn-menos" onclick="reducirAlCarrito(${item.id})"><i class="fa-solid fa-minus"></i></button>
-            </div>
-            <div class="item-carrito-subtotal">
-                <p class="item-subtotal">$${item.precio * item.cantidad} ARS</p>
-            </div>
-            <div class="item-carrito-borrar">
-                <button class="btn-carrito item-btn-remover" onclick="quitarDelCarrito(${item.id})"><i class="fa-solid fa-trash-can"></i></button>
-            </div>
-        </div>
-        
-      `;
+            
+          `;
         contenedorCarrito.innerHTML += itemHTML;
-    });
+    }
+
 }
 
 // Función para quitar productos del carrito
@@ -87,7 +120,23 @@ function quitarDelCarrito(id) {
     carrito = carrito.filter(item => item.id !== id);
     guardarCarritoEnLocalStorage();
     mostrarCarrito();
-    actualizarContadorCarrito()
+    actualizarContadorCarrito();
+    actualizarPrecioTotal();
+}
+
+function actualizarPrecioTotal() {
+    var valor = 0;
+    var precio = document.getElementById('precio');
+    precio.innerText = '';
+
+    if (carrito.length > 0) {
+        carrito.forEach(item => {
+            valor += item.precio * item.cantidad;
+        });
+        precio.innerText = '$ ' + valor + ' ARS*';
+    } else {
+        precio.innerText = '$ 0 ARS';
+    }
 }
 
 // Función para vaciar el carrito
@@ -95,7 +144,8 @@ function vaciarCarrito() {
     carrito = [];
     guardarCarritoEnLocalStorage();
     mostrarCarrito();
-    actualizarContadorCarrito()
+    actualizarContadorCarrito();
+    actualizarPrecioTotal();
 }
 
 // ------------ Evento para cargar el carrito al cargar la página
@@ -104,4 +154,5 @@ window.onload = function () {
     cargarCarritoDesdeLocalStorage();
     actualizarContadorCarrito();
     mostrarCarrito();
+    actualizarPrecioTotal();
 };
